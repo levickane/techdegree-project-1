55562207c7394a7c0685f2826719e2a3f8e0456a
levickane <oneleggedlevi09@gmail.com>
2020-05-28 13:35:05 -0500

    what I've got so far
    it will return a message that says undefined for both te quote and the source. can't figure out why

--- js/script.js
+++ js/script.js
@@ -6,24 +6,55 @@ project 1 - A Random Quote Generator
 // For assistance: 
   // Check the "Project Resources" section of the project instructions
   // Reach out in your Slack community - https://treehouse-fsjs-102.slack.com/app_redirect?channel=chit-chat
-
+console.log('test');
 /*** 
  * `quotes` array 
 ***/
-
-
+var quotes = [
+  {
+    quote: 'The greatest glory in living lies not in never falling, but in rising every time we fall.',
+    source: 'Nelson Mandela'
+  },
+  {
+    quote: 'The way to get started is to quit talking and begin doing.',
+    source: 'Walt Disney'
+  },
+  {
+    quote: "Your time is limited, so don't waste it living someone else's life. Don't be trapped by dogma – which is living with the results of other people's thinking.",
+    source: 'Steve Jobs'
+  },
+  {
+    quote: "Whats up doc?!",
+    source: "Buggs Bunny"
+  },
+  {
+    quote: "Give me liberty or give me death",
+    source: "Patrick Henry",
+    citation: "Second Virginia Convention",
+    year: "1775"
+  }
+];
+console.log(quotes)
 
 /***
  * `getRandomQuote` function
 ***/
-
-
+function getRandomQuote(quotes){
+  var randomNumber = Math.floor (Math.random() * quotes.length);  
+  return randomNumber
+};
+console.log(getRandomQuote(quotes));
 
 /***
  * `printQuote` function
 ***/
+let randomQuote = getRandomQuote(quotes);
+var html = `<p class="quote">${randomQuote.quote}</p>`;
+    html += `<p class="source">${randomQuote.source}</p>`;
 
-
+function printQuote(message){
+  document.getElementById('quote-box').innerHTML = html; 
+}
 
 /***
  * click event listener for the print quote button
